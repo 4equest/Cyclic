8319c9f90a0a5f3b9e1ca761c5a519a88d58840a
4equest <4equest@users.noreply.github.com>
2024-11-28 12:21:20 +0000

    deploy: edd837cbd9b454a042b83a5a293ceb5429a5f84b

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const STAGES = [
-    { rows: 1, cols: 3, bgColor: 0xe1f7fb, panelColor: 0x91cfdc, panelBgColor: 0xa8b0e0, name: "I" },
-    { rows: 2, cols: 2, bgColor: 0xffcc88, panelColor: 0xff9933, panelBgColor: 0xfff2e0, name: "II" },
-    { rows: 3, cols: 3, bgColor: 0xccff88, panelColor: 0x99cc33, panelBgColor: 0xe0ffd0, name: "III" },
-  ];
-  
-  export const PANEL_SIZE = 100;
-  
