70cc66dffd2f2271335443db9e86d5c7c4c51652
4equest <107108812+4equest@users.noreply.github.com>
2024-11-28 21:31:06 +0900

    lint?

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -65,23 +65,23 @@ export default class GameScene extends Phaser.Scene {
     const startX = (this.scale.width - cols * PANEL_SIZE) / 2;
     const startY = (this.scale.height - rows * PANEL_SIZE) / 2 + (cols * PANEL_SIZE / 2);
     console.log(rows, cols);
-    this.add.polygon(startX - PANEL_SIZE/2, startY - PANEL_SIZE*(cols-1) - PANEL_SIZE/2, [0, 0, 20, 0, 20, 20, 15, 20, 15, 5, 0, 5], this.stageConfig.panelColor).setAngle(270);
-    this.add.polygon(startX + PANEL_SIZE*(cols-1) + PANEL_SIZE/2, startY + PANEL_SIZE*(rows-1) + PANEL_SIZE/2, [0, 0, 20, 0, 20, 20, 15, 20, 15, 5, 0, 5], this.stageConfig.panelColor).setAngle(90);
+    this.add.polygon(startX - PANEL_SIZE / 2, startY - PANEL_SIZE * (cols - 1) - PANEL_SIZE / 2, [0, 0, 20, 0, 20, 20, 15, 20, 15, 5, 0, 5], this.stageConfig.panelColor).setAngle(270);
+    this.add.polygon(startX + PANEL_SIZE * (cols - 1) + PANEL_SIZE / 2, startY + PANEL_SIZE * (rows - 1) + PANEL_SIZE / 2, [0, 0, 20, 0, 20, 20, 15, 20, 15, 5, 0, 5], this.stageConfig.panelColor).setAngle(90);
 
-    this.add.rectangle(startX - PANEL_SIZE*0.75 + 5 , startY + PANEL_SIZE*0.75 + 5 + PANEL_SIZE*(rows-1), PANEL_SIZE/2, PANEL_SIZE/2, this.stageConfig.panelBgColor);
-    this.add.rectangle(startX - PANEL_SIZE*1.25 + 5 , startY + PANEL_SIZE*1.25 + 5 + PANEL_SIZE*(rows-1), PANEL_SIZE/4, PANEL_SIZE/4, this.stageConfig.panelBgColor);
-    this.add.rectangle(startX - PANEL_SIZE*1.55 + 5 , startY + PANEL_SIZE*1.55 + 5 + PANEL_SIZE*(rows-1), PANEL_SIZE/8, PANEL_SIZE/8, this.stageConfig.panelBgColor);
+    this.add.rectangle(startX - PANEL_SIZE * 0.75 + 5, startY + PANEL_SIZE * 0.75 + 5 + PANEL_SIZE * (rows - 1), PANEL_SIZE / 2, PANEL_SIZE / 2, this.stageConfig.panelBgColor);
+    this.add.rectangle(startX - PANEL_SIZE * 1.25 + 5, startY + PANEL_SIZE * 1.25 + 5 + PANEL_SIZE * (rows - 1), PANEL_SIZE / 4, PANEL_SIZE / 4, this.stageConfig.panelBgColor);
+    this.add.rectangle(startX - PANEL_SIZE * 1.55 + 5, startY + PANEL_SIZE * 1.55 + 5 + PANEL_SIZE * (rows - 1), PANEL_SIZE / 8, PANEL_SIZE / 8, this.stageConfig.panelBgColor);
 
-    this.add.rectangle(startX + cols * PANEL_SIZE - PANEL_SIZE*0.25 + 5, startY - cols * PANEL_SIZE + PANEL_SIZE*0.25 + 5, PANEL_SIZE/2, PANEL_SIZE/2, this.stageConfig.panelBgColor);
-    this.add.rectangle(startX + cols * PANEL_SIZE + PANEL_SIZE*0.25 + 5, startY - cols * PANEL_SIZE - PANEL_SIZE*0.25 + 5, PANEL_SIZE/4, PANEL_SIZE/4, this.stageConfig.panelBgColor);
-    this.add.rectangle(startX + cols * PANEL_SIZE + PANEL_SIZE*0.55 + 5, startY - cols * PANEL_SIZE - PANEL_SIZE*0.55 + 5, PANEL_SIZE/8, PANEL_SIZE/8, this.stageConfig.panelBgColor);
+    this.add.rectangle(startX + cols * PANEL_SIZE - PANEL_SIZE * 0.25 + 5, startY - cols * PANEL_SIZE + PANEL_SIZE * 0.25 + 5, PANEL_SIZE / 2, PANEL_SIZE / 2, this.stageConfig.panelBgColor);
+    this.add.rectangle(startX + cols * PANEL_SIZE + PANEL_SIZE * 0.25 + 5, startY - cols * PANEL_SIZE - PANEL_SIZE * 0.25 + 5, PANEL_SIZE / 4, PANEL_SIZE / 4, this.stageConfig.panelBgColor);
+    this.add.rectangle(startX + cols * PANEL_SIZE + PANEL_SIZE * 0.55 + 5, startY - cols * PANEL_SIZE - PANEL_SIZE * 0.55 + 5, PANEL_SIZE / 8, PANEL_SIZE / 8, this.stageConfig.panelBgColor);
 
 
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         const x = startX + j * PANEL_SIZE;
         const y = startY + i * PANEL_SIZE - j * PANEL_SIZE;
-        const panel_bg = this.add.rectangle(x+5, y+5, PANEL_SIZE - 5, PANEL_SIZE - 5, this.stageConfig.panelBgColor);
+        const panel_bg = this.add.rectangle(x + 5, y + 5, PANEL_SIZE - 5, PANEL_SIZE - 5, this.stageConfig.panelBgColor);
 
         const panel = this.add.rectangle(x, y, PANEL_SIZE - 5, PANEL_SIZE - 5, this.stageConfig.panelColor)
           .setInteractive();
@@ -232,7 +232,7 @@ export default class GameScene extends Phaser.Scene {
       this.panels.forEach(panel => {
         panel.disableInteractive(); // パネルの操作を無効化
       });
-      
+
       const elapsed = Math.floor((Date.now() - this.startTime) / 1000);
       this.add.text(200, 300, `Time: ${elapsed}`, {
         fontSize: '32px',
@@ -260,8 +260,8 @@ export default class GameScene extends Phaser.Scene {
     const size = Phaser.Math.Between(10, 30);
 
     const effect = this.add.rectangle(x, y, size, size, this.stageConfig.panelBgColor).setAngle(45);
-    const effect_2 = this.add.rectangle(x, y + size*1.2, size/2, size/2, this.stageConfig.panelBgColor).setAngle(45);
-    const effect_3 = this.add.rectangle(x, y + size*2, size/2, size/2, this.stageConfig.panelBgColor).setAngle(45);
+    const effect_2 = this.add.rectangle(x, y + size * 1.2, size / 2, size / 2, this.stageConfig.panelBgColor).setAngle(45);
+    const effect_3 = this.add.rectangle(x, y + size * 2, size / 2, size / 2, this.stageConfig.panelBgColor).setAngle(45);
 
     this.tweens.add({
       targets: [effect, effect_2, effect_3],
